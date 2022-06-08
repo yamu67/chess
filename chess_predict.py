8dbad880a28e608e97331d2da63371a755ee797e
yamu67 <107048196+yamu67@users.noreply.github.com>
2022-06-08 11:55:50 +0530

    Update chess_predict.py

--- chess_predict.py
+++ chess_predict.py
@@ -11,7 +11,7 @@ st.text("Provide URL of flower Image for image classification")
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('./Chess Piece Prediction')
+  model = tf.keras.models.load_model('./chessmodel')
   return model
 
 with st.spinner('Loading Model Into Memory....'):
